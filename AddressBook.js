50918d03e62c9c0df12dcc1031bbc8cb185060a5
Mrk0p <sachinwele23@gmail.com>
2023-03-08 13:18:21 +0530

    Added:ensure valid contact

--- AddressBook.js
+++ AddressBook.js
@@ -64,16 +64,27 @@ class Contact {
 }
 
 const prompt = require("prompt-sync")({sigint:true}); 
-let first_name = prompt("Eter the first name : ");
-let last_name = prompt("Enter the last name : ");
-let address = prompt("Enter the address : ");
-let city = prompt("Enter the city name : ");
-let state = prompt("Enter the state name : ");
-let zip = prompt("Enter the zip code : ");
-let phone_no = prompt("Enter the phone number : ");
-let email = prompt("Enter the email address : ");
+let first_name = getUserInput(prompt("Eter the first name : "), "^[A-Z][a-zA-Z]{2,}");
+let last_name = getUserInput(prompt("Enter the last name : "), "^[A-Z][a-zA-Z]{2,}");
+let address = getUserInput(prompt("Enter the address : "), "[a-zA-Z\w\d\s]{3,}");
+let city = getUserInput(prompt("Enter the city name : "), "[a-zA-Z\w\d\s]{3,}");
+let state = getUserInput(prompt("Enter the state name : "), "[a-zA-Z\w\d\s]{3,}");
+let zip = getUserInput(prompt("Enter the zip code : "), "[0-9]{6}");
+let phone_no = getUserInput(prompt("Enter the phone number : "), "^(91)?([ ])?[6-9]{1}[0-9]{9}$");
+let email = getUserInput(prompt("Enter the email address : "), "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@([a-z1-9]+)([.][a-z]*)?(\.[a-z]{2,})$");
 function createContact() {
     let contact = new Contact(first_name, last_name, address, city, state, zip, phone_no, email);
     console.log(contact.toString());
+}
+function getUserInput(value, regexPattern) {
+    let regex = RegExp(regexPattern);
+    if(regex.test(value)) {
+        return value;
+    }
+    else {
+        console.log("Invalid input");
+        value = prompt("Enter valid input");
+        getUserInput(value, regexPattern);
+    }
 }
 createContact();
